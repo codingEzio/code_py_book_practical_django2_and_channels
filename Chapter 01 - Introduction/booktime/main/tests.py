3e6e2a0731812ac9e870643d41c1f4f902aaccf2
Ezio <assassinste@gmail.com>
2019-02-28 12:36:47 +0800

    A tiny test file for 'booktime::main::home.html'

--- Chapter 01 - Introduction/booktime/main/tests.py
+++ Chapter 01 - Introduction/booktime/main/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestPage(TestCase):
+    def test_home_page_works(self):
+        """
+        Frankly, these comments aren't necessary for me.
+        But.. Just in case I forgot this, I'll put some notes in here.
+
+        `assertContains`    Does the page contains strings like 'BookTime'?
+        """
+
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "BookTime")
