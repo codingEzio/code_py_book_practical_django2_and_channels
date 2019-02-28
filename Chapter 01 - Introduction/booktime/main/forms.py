6e132ef32ecd087e4e307ec8d2e5bb92522be771
Ezio <assassinste@gmail.com>
2019-02-28 21:34:51 +0800

    Configure 'sending-email' itself

--- Chapter 01 - Introduction/booktime/main/forms.py
+++ Chapter 01 - Introduction/booktime/main/forms.py
@@ -1,4 +1,10 @@
+import logging
+
 from django import forms
+from django.core.mail import send_mail
+
+
+logging = logging.getLogger(__name__)
 
 
 class ContactForm(forms.Form):
@@ -6,3 +12,20 @@ class ContactForm(forms.Form):
     message = forms.CharField(
         max_length=600, widget=forms.Textarea
     )
+
+    def send_mail(self):
+        logging.info("Sending email to customr service ..")
+
+        message = "From: {0}\n{1}".format(
+            self.cleaned_data["name"],
+            self.cleaned_data["message"]    
+        )
+
+        send_mail(
+            "Site message",
+            message,
+            "site@booktime.domain",
+            ["customerservice@booktime.domain"],
+            fail_silently=False,
+        )
+    
