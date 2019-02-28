409b9b3ad44c00d807a3adbff982b711d27b127a
Ezio <assassinste@gmail.com>
2019-02-28 15:36:43 +0800

    Decoupling the 'request-URL' while testing

--- Chapter 01 - Introduction/booktime/main/tests.py
+++ Chapter 01 - Introduction/booktime/main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 class TestPage(TestCase):
@@ -8,16 +9,17 @@ class TestPage(TestCase):
         But.. Just in case I forgot this, I'll put some notes in here.
 
         `assertContains`    Does the page contains strings like 'BookTime'?
+        `reverse("home")`   The "home" is the `name` attr in the `urlpatterns`.
         """
 
-        response = self.client.get("/")
+        response = self.client.get(reverse("home"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "BookTime")
 
     def test_about_page_works(self):
-        response = self.client.get("/about-us/")
+        response = self.client.get(reverse("about_us"))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "about_us.html")
--- Chapter 01 - Introduction/booktime/main/urls.py
+++ Chapter 01 - Introduction/booktime/main/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("about-us/",
-         TemplateView.as_view(template_name="about_us.html")),
+         TemplateView.as_view(template_name="about_us.html"), ),
     path("",
-         TemplateView.as_view(template_name="home.html")),
+         TemplateView.as_view(template_name="home.html"), ),
 ]
