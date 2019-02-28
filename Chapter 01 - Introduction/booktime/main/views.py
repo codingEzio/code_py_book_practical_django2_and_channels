c8faa4c5ad10646d4f5aed00f4ad546b7ef42d9e
Ezio <assassinste@gmail.com>
2019-02-28 21:38:14 +0800

    Setup our 'contact-form' page (writestuff => submit)

--- Chapter 01 - Introduction/booktime/main/views.py
+++ Chapter 01 - Introduction/booktime/main/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
 
-# Create your views here.
+from main import forms
+
+
+class ContactUsView(FormView):
+    """
+    What if you wanna write this in 'Function-based' way?
+
+        def contact_us(request):
+            if request.method == "POST":
+                form = forms.ContactForm(request.POST)
+
+                if form.is_valid():
+                    form.send_mail()
+                    return HttpResponseRedirect("/")
+            else:
+                form = forms.ContactForm()
+
+            return render(request, "contact_form.html", {"form": form})
+
+    Okay.. Whatever :P
+    """
+
+    template_name = "contact_form.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
