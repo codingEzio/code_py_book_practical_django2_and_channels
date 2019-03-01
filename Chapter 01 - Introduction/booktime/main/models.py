c84260319bc2d336e1e64724c9927be9bbc7eff0
Ezio <assassinste@gmail.com>
2019-03-01 09:58:39 +0800

    DB fields for 'Product Tag' added (& synced-to-DB)

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -22,3 +22,11 @@ class ProductImage(models.Model):
         Product, on_delete=models.CASCADE
     )
     image = models.ImageField(upload_to="product-images")
+
+
+class ProductTag(models.Model):
+    products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.SlugField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
