187ca926169d7908ebf252fb2b0ceee8899dde9c
Ezio <assassinste@gmail.com>
2019-03-03 19:17:22 +0800

    A simple '__str__' for 'ProductImage'

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -55,3 +55,6 @@ class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="product-images")
     thumbnail = models.ImageField(upload_to="product-thumbnails", null=True)
+
+    def __str__(self):
+        return self.product.name
