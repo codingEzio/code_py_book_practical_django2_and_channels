26e848c2661c180f9a18789b4bf50dd32d7eee1c
Ezio <assassinste@gmail.com>
2019-03-03 11:32:02 +0800

    Little update for 'gen thumbnails' (with WHY)

--- Chapter 01 - Introduction/booktime/main/signals.py
+++ Chapter 01 - Introduction/booktime/main/signals.py
@@ -17,14 +17,24 @@ logger = logging.getLogger(__name__)
 
 @receiver(pre_save, sender=ProductImage)
 def generate_thumbnail(sender, instance, **kwargs):
+    """
+    For the sake of simplicity,
+    I used the examples from the official documentation.
+        https://pillow.readthedocs.io/en/stable/reference/Image.html#create-thumbnails
+    
+        OLD     image_inst = image_inst.convert("RGB")
+                image_inst.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+                
+        NEW     image_inst.thumbnail(THUMBNAIL_SIZE)
+    """
+    
     logger.info("Generating thumbnail for product %2d", instance.product.id)
 
-    image = Image.open(instance.image)
-    image = image.convert("RGB")
-    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+    image_inst = Image.open(instance.image)
+    image_inst.thumbnail(THUMBNAIL_SIZE)
 
     temp_thumb = BytesIO()
-    image.save(temp_thumb, "JPEG")
+    image_inst.save(temp_thumb, "JPEG")
 
     temp_thumb.seek(0)
 
