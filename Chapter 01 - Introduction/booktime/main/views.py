a42aa8f4f1c1a5ca1d39a23d0e69c894a86b4fd7
Ezio <assassinste@gmail.com>
2019-03-04 19:52:41 +0800

    The core logic code for 'display-product-list'

--- Chapter 01 - Introduction/booktime/main/views.py
+++ Chapter 01 - Introduction/booktime/main/views.py
@@ -1,6 +1,31 @@
 from django.views.generic.edit import FormView
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 
-from main import forms
+from main import forms, models
+
+
+class ProductListView(ListView):
+    template_name = "main/product_list.html"
+    paginate_by = 4
+
+    def get_queryset(self):
+        """
+        Ah, the two `tag`s here are NOT the same.
+        """
+
+        tag = self.kwargs["tag"]
+        self.tag = None
+
+        if tag != "all":
+            self.tag = get_object_or_404(models.ProductTag, slug=tag)
+
+        if self.tag:
+            products = models.Product.objects.active().filter(tags=self.tag)
+        else:
+            products = models.Product.objects.active()
+
+        return products.order_by("name")
 
 
 class ContactUsView(FormView):
