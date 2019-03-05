281f225d63d1fc1a7cad9b2755aa45b2829e2b50
Ezio <assassinste@gmail.com>
2019-03-05 17:57:06 +0800

    Customizing 'User' model (it's a model as well!)

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
+
 
 class ActiveManager(models.Manager):
     def active(self):
@@ -58,3 +63,60 @@ class ProductImage(models.Model):
 
     def __str__(self):
         return self.product.name
+
+
+class UserManager(BaseUserManager):
+    """
+    Q & A
+        is_staff            able to access the admin site
+        is_superuser        able to do .. anything
+        normalize_email     lowercasing the "domain" part
+    """
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The given email must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_admin", False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have `is_stuff=True`.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have `is_superuser=True`.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    """
+    Q & A
+        USERNAME_FIELD      it could any fields (mostly 'CharField', 'EmailField', though)
+        REQUIRED_FIELDS     prompted for when creating a user via the `createsuperuser` (huh)
+    """
+
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"    # ↑ the `email` field above ↑
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()     # override some of methods we needed
