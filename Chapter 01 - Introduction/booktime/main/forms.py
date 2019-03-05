3b4e34247ffa094dfa41961ff33c601641600bf9
Ezio <assassinste@gmail.com>
2019-03-05 20:01:57 +0800

    A little modification for 'UserCreationForm'

--- Chapter 01 - Introduction/booktime/main/forms.py
+++ Chapter 01 - Introduction/booktime/main/forms.py
@@ -4,7 +4,15 @@ from django import forms
 from django.core.mail import send_mail
 
 
-logging = logging.getLogger(__name__)
+from django.contrib.auth.forms import (
+    UserCreationForm as OurUserCreationForm
+)
+from django.contrib.auth.forms import UsernameField
+
+from . import models
+
+
+logger = logging.getLogger(__name__)
 
 
 class ContactForm(forms.Form):
@@ -14,7 +22,7 @@ class ContactForm(forms.Form):
     )
 
     def send_mail(self):
-        logging.info("Sending email to customr service ..")
+        logger.info("Sending email to customr service ..")
 
         message = "From: {0}\n{1}".format(
             self.cleaned_data["name"],
@@ -28,4 +36,43 @@ class ContactForm(forms.Form):
             ["customerservice@booktime.domain"],
             fail_silently=False,
         )
-    
+
+
+class UserCreationForm(OurUserCreationForm):
+    class Meta(OurUserCreationForm.Meta):
+        """
+        A little modification due to changes in 'model' & 'admin' site.
+
+        Specifically, I'm talkin' about the `fields` & `field_classes`.
+        Since we've change the 'name+pwd' to 'mail+pwd'
+            We'll need to override the default one ( aka `UserCreationForm` )
+
+        By the way,
+            the `field_classes` is just <use> the `fields`
+            Those two are <together> :D
+        """
+
+        model = models.User
+        fields = ("email", )
+        field_classes = {"email": UsernameField}
+
+    def send_mail(self):
+        """
+        Cuz you <need> to send <registration info>.
+        That means you <need> this function (not the other reasons, apparently).
+        """
+
+        logger.info(
+            "Sending signup email for email=%s",
+            self.cleaned_data["email"],
+        )
+        message = "Welcome{}".format(self.cleaned_data["email"])
+
+        # TODO as a marker (the 'email' might need to change!)
+        send_mail(
+            "Welcome to BookTime",
+            message,
+            "admin@example.com",
+            [self.cleaned_data["email"]],
+            fail_silently=True,
+        )
