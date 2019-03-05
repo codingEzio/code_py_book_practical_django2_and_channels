7c25795d3536bb5bc7613ed4c6ec86480f2786fa
Ezio <assassinste@gmail.com>
2019-03-05 20:03:33 +0800

    The core logic for 'user-signup-page' (redirect, clean, auth etc.)

--- Chapter 01 - Introduction/booktime/main/views.py
+++ Chapter 01 - Introduction/booktime/main/views.py
@@ -1,8 +1,15 @@
+import logging
+
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+
 from main import forms, models
+
+logger = logging.getLogger(__name__)
 
 
 class ProductListView(ListView):
@@ -54,3 +61,55 @@ class ContactUsView(FormView):
     def form_valid(self, form):
         form.send_mail()
         return super().form_valid(form)
+
+
+class SignupView(FormView):
+    """
+    This is actually two parts.
+    -- One for 'registration' page
+    -- One for 'sending email' feature
+
+    The `FormView` is quite special here.
+    1. It's one of the `ModelView` type.
+    2. The <data submitted> can be automatically stored in a model.
+
+    According to the author,
+        the `authenticate` will ALWAYS work in the same way
+        even if you're using some other auth methods (DB-based, LDAP etc.)
+
+    And the `messages`
+        it was used to "flash" a message to users (after doing/finished sth).
+
+        e.g.
+            https://www.w3schools.com/bootstrap/bootstrap_alerts.asp
+            https://codepen.io/danrosenthal/pen/BWyORL?html-preprocessor=slim (complex)
+    """
+
+    template_name = "signup.html"
+    form_class = forms.UserCreationForm
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+
+        email = form.cleaned_data.get("email")
+        raw_password = form.cleaned_data.get("password1")
+
+        logger.info(
+            "New signup for email=%s through SignupView", email
+        )
+
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+
+        form.send_mail()
+
+        messages.info(
+            self.request, "You signed up successfully :D"
+        )
+
+        return response
