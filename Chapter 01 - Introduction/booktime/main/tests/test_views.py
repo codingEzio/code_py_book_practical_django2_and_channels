e3c5b852c0255d2632ae0084780469586b331f1c
Ezio <assassinste@gmail.com>
2019-03-05 20:44:27 +0800

    Tiny tests for 'does-it {success_msg, load_correctly, auth_correctly}'

--- Chapter 01 - Introduction/booktime/main/tests/test_views.py
+++ Chapter 01 - Introduction/booktime/main/tests/test_views.py
@@ -1,10 +1,13 @@
+import sys
 from decimal import Decimal
+from unittest.mock import patch
 
 from django.test import TestCase
 from django.urls import reverse
 
+from django.contrib import auth
+
 from main import forms, models
-import sys
 
 
 class TestPage(TestCase):
@@ -116,3 +119,44 @@ class TestPage(TestCase):
             list(response.context["object_list"]),
             list(product_list),
         )
+
+    def test_user_signup_page_loads_correctly(self):
+        """
+        """
+        response = self.client.get(reverse("main:signup"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "signup.html")
+        self.assertContains(response, "BookTime")
+        self.assertIsInstance(
+            response.context["form"], forms.UserCreationForm
+        )
+
+
+    def test_user_signup_page_submission_works(self):
+        """
+        """
+
+        post_data = {
+            "email": "user@domain.com",
+            "password1": "abcabcabc",
+            "password2": "abcabcabc",
+        }
+
+        with patch.object(forms.UserCreationForm, "send_mail") as mock_send:
+            response = self.client.post(
+                reverse("main:signup"), post_data
+            )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(
+            models.User.objects.filter(
+                email="user@domain.com"
+            ).exists()
+        )
+
+        self.assertTrue(
+            auth.get_user(self.client).is_authenticated
+        )
+
+        mock_send.assert_called_once()
