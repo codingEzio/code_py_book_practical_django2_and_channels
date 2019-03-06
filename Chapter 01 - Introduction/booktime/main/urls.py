e1293d03ecc0d135b67d49ad17e6233256d08fc8
Ezio <assassinste@gmail.com>
2019-03-06 12:43:08 +0800

    Routes for 'login' page

--- Chapter 01 - Introduction/booktime/main/urls.py
+++ Chapter 01 - Introduction/booktime/main/urls.py
@@ -2,25 +2,29 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 
-from main import views, models
+from django.contrib.auth import views as auth_views
+
+from main import views, models, forms
 
 app_name = "main"
 
-
 urlpatterns = [
-     path("product/<slug:slug>/",
-          DetailView.as_view(model=models.Product), name="product"),
-     path("products/<slug:tag>/",
-          views.ProductListView.as_view(), name="products"),
+    path("product/<slug:slug>/",
+         DetailView.as_view(model=models.Product), name="product"),
+    path("products/<slug:tag>/",
+         views.ProductListView.as_view(), name="products"),
 
-     path("signup/",
-          views.SignupView.as_view(), name="signup"),
+    path("signup/",
+         views.SignupView.as_view(), name="signup"),
+    path("login/",
+         auth_views.LoginView.as_view(template_name="login.html",
+                                      form_class=forms.AuthenticationForm), name="login"),
 
-     path("contact-us/",
-          views.ContactUsView.as_view(), name="contact_us"),
-     path("about-us/",
-          TemplateView.as_view(template_name="about_us.html"), name="about_us"),
+    path("contact-us/",
+         views.ContactUsView.as_view(), name="contact_us"),
+    path("about-us/",
+         TemplateView.as_view(template_name="about_us.html"), name="about_us"),
 
-     path("",
-          TemplateView.as_view(template_name="home.html"), name="home"),
+    path("",
+         TemplateView.as_view(template_name="home.html"), name="home"),
 ]
