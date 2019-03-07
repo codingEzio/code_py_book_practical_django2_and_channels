d9ea240d5fe41e39a4b743698748e2509e984314
Ezio <assassinste@gmail.com>
2019-03-07 18:20:15 +0800

    Routes for users to CRUD their addresses

--- Chapter 01 - Introduction/booktime/main/urls.py
+++ Chapter 01 - Introduction/booktime/main/urls.py
@@ -14,6 +14,15 @@ urlpatterns = [
     path("products/<slug:tag>/",
          views.ProductListView.as_view(), name="products"),
 
+    path("address/",
+         views.AddressListView.as_view(), name="address_list"),
+    path("address/create/",
+         views.AddressCreateView.as_view(), name="address_create"),
+    path("address/<int:pk>/",
+         views.AddressUpdateView.as_view(), name="address_update"),
+    path("address/<int:pk>/delete/",
+         views.AddressDeleteView.as_view(), name="address_delete"),
+
     path("signup/",
          views.SignupView.as_view(), name="signup"),
     path("login/",
