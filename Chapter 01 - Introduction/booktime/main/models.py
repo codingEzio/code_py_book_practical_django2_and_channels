aa5afeb0d9eee2c595e09bacc1cb4885c41d423f
Ezio <assassinste@gmail.com>
2019-03-07 18:04:42 +0800

    Tiny typo fixed (oops)

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -89,7 +89,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_superuser", False)
 
         return self._create_user(email, password, **extra_fields)
 
