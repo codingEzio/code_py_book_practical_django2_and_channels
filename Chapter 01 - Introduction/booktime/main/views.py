f5e17881bc3b7c1613dec1b31ed35e8a607e9675
Ezio <assassinste@gmail.com>
2019-03-07 18:19:53 +0800

    Implementing 'letting-user-CRUD-their-addresses'

--- Chapter 01 - Introduction/booktime/main/views.py
+++ Chapter 01 - Introduction/booktime/main/views.py
@@ -1,9 +1,16 @@
 import logging
 
-from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.views.generic.edit import (
+    FormView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 
@@ -113,3 +120,52 @@ class SignupView(FormView):
         )
 
         return response
+
+
+
+ADDRESS_FIELDS = [
+    "name",
+    "address1",
+    "address2",
+    "zip_code",
+    "city",
+    "country",
+]
+
+
+class AddressListView(LoginRequiredMixin, ListView):
+    model = models.Address
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressCreateView(LoginRequiredMixin, CreateView):
+    model = models.Address
+    fields = ADDRESS_FIELDS
+    success_url = reverse_lazy("main:address_list")
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+
+        obj.user = self.request.user
+        obj.save()
+
+        return super().form_valid(form)
+
+
+class AddressUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Address
+    fields = ADDRESS_FIELDS
+    success_url = reverse_lazy("main:address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
+
+
+class AddressDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Address
+    success_url = reverse_lazy("main:address_list")
+
+    def get_queryset(self):
+        return self.model.objects.filter(user=self.request.user)
