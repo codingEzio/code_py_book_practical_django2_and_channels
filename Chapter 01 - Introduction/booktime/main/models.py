ca6f6e0959a2374f5f5abe621e7a77c9ccc90b2c
Ezio <assassinste@gmail.com>
2019-03-07 15:34:31 +0800

    New model added for <Users' addresses>

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -120,3 +120,33 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()     # override some of methods we needed
+
+
+class Address(models.Model):
+    """
+    We'll need this info later when we build the 'checkout' system.
+    """
+
+    SUPPORTED_COUNTRIES = (
+        ("uk", "United Kingdom"),
+        ("us", "United States of America"),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=60)
+    address1 = models.CharField("Address line 1", max_length=60)
+    address2 = models.CharField("Address line 2", max_length=60, blank=True)
+
+    zip_code = models.CharField("ZIP / Postal code", max_length=12)
+    city = models.CharField(max_length=60)
+    country = models.CharField(max_length=3, choices=SUPPORTED_COUNTRIES)
+
+    def __str__(self):
+        return ", ".join([
+            self.name,
+            self.address1,
+            self.address2,
+            self.zip_code,
+            self.city,
+            self.country,
+        ])
