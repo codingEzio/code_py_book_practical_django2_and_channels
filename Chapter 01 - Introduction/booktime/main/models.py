a075dfbc29fcbab79e00347b50eb1492c7530d5b
Ezio <assassinste@gmail.com>
2019-03-11 09:29:35 +0800

    Method for creating orders (inside 'Basket')

--- Chapter 01 - Introduction/booktime/main/models.py
+++ Chapter 01 - Introduction/booktime/main/models.py
@@ -191,6 +191,91 @@ class Basket(models.Model):
     def count(self):
         return sum(i.quantity for i in self.basketline_set.all())
 
+    def create_order(self, billing_address, shipping_address):
+        """
+        #TODO break down this method
+        """
+
+        # Being logged in is required
+        if not self.user:
+            raise exceptions.BasketException(
+                "Cannot create order without user!"
+            )
+
+        logger.info(
+            "Creating order for basket_id=%2d"
+            ", shipping_address_id=%2d, billing_address_id=%2d",
+            self.id,
+            shipping_address.id,
+            billing_address.id,
+        )
+
+        # Assigning values to the fields of 'Order' model
+        #   what I'm really interested is the latter, `VARIABLE.ATTRIBUTE`.
+        order_data = {
+            "user": self.user,
+            "billing_name": billing_address.name,
+            "billing_address1": billing_address.address1,
+            "billing_address2": billing_address.address2,
+            "billing_zip_code": billing_address.zip_code,
+            "billing_city": billing_address.city,
+            "billing_country": billing_address.country,
+            "shipping_name": shipping_address.name,
+            "shipping_address1": shipping_address.address1,
+            "shipping_address2": shipping_address.address2,
+            "shipping_zip_code": shipping_address.zip_code,
+            "shipping_city": shipping_address.city,
+            "shipping_country": shipping_address.country,
+        }
+
+        # Nonetheless, the 'order' was created at this line
+        order = Order.objects.create(**order_data)
+
+        c = 0
+
+        # All records of the `BasketLine' table
+        for line in self.basketline_set.all():
+
+            for item in range(line.quantity):
+
+                # This line produces these
+                # || order   info : {user, billing_info, shipping_info}
+                # || product info : {product(id) -- looping outside by 'quantity'}
+                order_line_data = {
+                    "order": order,
+                    "product": line.product,
+                }
+
+                # Creating `OrderLine` inst by {
+                #   essential-ones          product-name/id  (quantity: outside loop)
+                #   not-so-essential-ones   user, shipping/billing info
+                # }
+                order_line = OrderLine.objects.create(
+                    **order_line_data
+                )
+
+                # Produces total num
+                # e.g. (=> 4)
+                #   2* prod-one
+                #   1* prod-one
+                #   1* prod-one-another-basket
+                c += 1
+
+        logger.info(
+            "Created order with id=%2d and lines_count=%2d",
+            order.id,
+            c,
+        )
+
+        # Checkout (Basket => Submitted)
+        self.status = Basket.SUBMITTED
+
+        self.save()
+
+        return order
+
+
+
 class BasketLine(models.Model):
     basket = models.ForeignKey(Basket, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
