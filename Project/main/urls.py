62940fa5bf9cfa0b8dd1012a6fac1ffc667fe4a4
Ezio <assassinste@gmail.com>
2019-03-13 10:21:08 +0800

    Routes for 'order dashboard' page

--- Project/main/urls.py
+++ Project/main/urls.py
@@ -24,6 +24,9 @@ urlpatterns = [
     path("order/address_select/",
          views.AddressSelectionView.as_view(), name="address_select"),
 
+    path("order-dashboard/",
+         views.OrderView.as_view(), name="order_dashboard"),
+
     path("address/",
          views.AddressListView.as_view(), name="address_list"),
     path("address/create/",
